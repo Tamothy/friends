706697ad91916e7a6ab6ec260c9bfa21a7073b0d
Cecilia Le Brech and Tam Nguyen <cecilialebrech@gmail.com>
2016-06-08 20:54:41 -0700

    add logical operators for friendsquestionnaire/user interface

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,51 @@
 $(document).ready(function() {
-  $("form#celebrity").submit(function(event) {
-    var age = parseInt($("input#age").val());
+  $("form#friends").submit(function(event) {
+    var mood = $("select#mood").val();
     var gender = $("select#gender").val();
 
 
-      if (gender === "male" && age < 21) {
-      $("#Miley").show();
-      $("#Scarlett").hide();
-      $("#Hillary").hide();
-      $("#Obama").hide();
-    } else if (gender === "male" && age >= 21 && age <= 30) {
-      $("#Scarlett").show();
-      $("#Miley").hide();
-      $("#Hillary").hide();
-      $("#Obama").hide();
-    } else if (gender === "male" && age > 30) {
-      $("#Hillary").show();
-      $("#Scarlett").hide();
-      $("#Miley").hide();
-      $("#Obama").hide();
-    } else if (gender === "female") {
-      $("#Obama").show();
-      $("#Hillary").hide();
-      $("#Scarlett").hide();
-      $("#Miley").hide();
-
-    } else {
-      alert("Please enter information correctly. Refresh and try again!");
+    if (gender === "male" && mood === "curious") {
+      $("#joey").show();
+      $("#phoebe").hide();
+      $("#ross").hide();
+      $("#chandler").hide();
+      $("#monica").hide();
+      $("#rachel").hide();
+    } else if (gender === "male" && mood === "sad") {
+      $("#joey").hide();
+      $("#phoebe").hide();
+      $("#ross").show();
+      $("#chandler").hide();
+      $("#monica").hide();
+      $("#rachel").hide();
+    } else if (gender === "male" && mood === "tidy") {
+      $("#joey").hide();
+      $("#phoebe").hide();
+      $("#ross").hide();
+      $("#chandler").show();
+      $("#monica").hide();
+      $("#rachel").hide();
+    } else if (gender === "female" && mood === "curious") {
+      $("#joey").hide();
+      $("#phoebe").show();
+      $("#ross").hide();
+      $("#chandler").hide();
+      $("#monica").hide();
+      $("#rachel").hide();
+    } else if (gender === "female" && mood === "sad") {
+      $("#joey").hide();
+      $("#phoebe").hide();
+      $("#ross").hide();
+      $("#chandler").hide();
+      $("#monica").hide();
+      $("#rachel").show();
+    } else if (gender === "female" && mood === "tidy") {
+      $("#joey").hide();
+      $("#phoebe").hide();
+      $("#ross").hide();
+      $("#chandler").hide();
+      $("#monica").show();
+      $("#rachel").hide();
     }
     event.preventDefault();
   });
